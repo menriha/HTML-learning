d025b438559b600534e6ddbabb02584043801570
Maxim Drobchak <drobchak1988@gmail.com>
2019-10-21 20:54:56 +0300

    starter learning

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ window.onload = () => {
   let second = 0;
   let minutes = 0;
   document.body.innerHTML =
-    '<header> <i id="timer-minutes"> </i>: <i id="timer-second"> </i></header>';
+    '<header> <a href="http://htmlbook.ru/" id="html-link" target="blank">htmlbook</a><i id="timer-minutes"> </i>: <i id="timer-second"> </i></header>';
   setInterval(() => {
     second++;
     second < 10
@@ -13,11 +13,10 @@ window.onload = () => {
       second = 0;
       minutes++;
     }
-    if (minutes > 0) {
-      minutes < 10
-        ? (document.getElementById("timer-minutes").innerHTML = "0" + minutes)
-        : (document.getElementById("timer-minutes").innerHTML = minutes);
-      document.getElementById("timer-minutes").innerHTML = minutes;
-    }
+
+    minutes < 10
+      ? (document.getElementById("timer-minutes").innerHTML = "0" + minutes)
+      : (document.getElementById("timer-minutes").innerHTML = minutes);
+    document.getElementById("timer-minutes").innerHTML = minutes;
   }, 1000);
 };
